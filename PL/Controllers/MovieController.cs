5dd772545f6659798818bf0838e5faa54230da72
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-31 15:50:47 -0600

    Delete

--- PL/Controllers/MovieController.cs
+++ PL/Controllers/MovieController.cs
@@ -41,6 +41,43 @@ namespace PL.Controllers
                 return View(resultMovi);
             }
         }
+        [HttpPost] //Añadir a Favoritos
+        public ActionResult Form(ML.Movie idPelicula)
+        {
+            if (idPelicula != null)
+            {
+                ML.Favorite favorite = new ML.Favorite();
+                favorite.media_id = idPelicula.id;
+                favorite.media_type = "movie";
+                favorite.favorite = true;
+
+                ML.Movie resultWebApi = new ML.Movie();
+                resultWebApi.Objects = new List<object>();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://api.themoviedb.org/3/account/19729267/");
+
+                    //HTTP POST
+                    var postTask = client.PostAsJsonAsync<ML.Favorite>("favorite?session_id=968c620908c4ff2f89b1adc20c5dfddb53112139&api_key=401526372779a487928a18f653d2ee6d", favorite);
+                    postTask.Wait();
+
+                    var resultUsuario = postTask.Result;
+                    if (resultUsuario.IsSuccessStatusCode)
+                    {
+
+                        return RedirectToAction("GetAll");
+
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Ocurrio un error al insertar el registro" + " ";
+                    }
+                    ViewBag.Message = "El resigistro de Usuario a sido agrgado con exito";
+                }
+            }
+
+            return View();
+        }
         public IActionResult GetFavorite()
         {
             ML.Movie resultMovi = new ML.Movie();
@@ -71,6 +108,43 @@ namespace PL.Controllers
 
                 return View(resultMovi);
             }
+        }
+        [HttpPost] //Añadir a Favoritos
+        public ActionResult FormDelete(ML.Movie idPelicula)
+        {
+            if (idPelicula != null)
+            {
+                ML.Favorite favorite = new ML.Favorite();
+                favorite.media_id = idPelicula.id;
+                favorite.media_type = "movie";
+                favorite.favorite = false;
+
+                ML.Movie resultWebApi = new ML.Movie();
+                resultWebApi.Objects = new List<object>();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://api.themoviedb.org/3/account/19729267/");
+
+                    //HTTP POST
+                    var postTask = client.PostAsJsonAsync<ML.Favorite>("favorite?session_id=968c620908c4ff2f89b1adc20c5dfddb53112139&api_key=401526372779a487928a18f653d2ee6d", favorite);
+                    postTask.Wait();
+
+                    var resultUsuario = postTask.Result;
+                    if (resultUsuario.IsSuccessStatusCode)
+                    {
+
+                        return RedirectToAction("GetAllFavorites");
+
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Ocurrio un error al insertar el registro" + " ";
+                    }
+                    ViewBag.Message = "El resigistro de Usuario a sido agrgado con exito";
+                }
+            }
+
+            return View();
         }
         //public IActionResult GetFavorite()
         //{
@@ -106,32 +180,32 @@ namespace PL.Controllers
 
 
 
-            //public IActionResult GetFavoritos()
-            //{
-            //    ML.Movie movi = new ML.Movie();
+        //public IActionResult GetFavoritos()
+        //{
+        //    ML.Movie movi = new ML.Movie();
 
-            //    using (var cliente = new HttpClient())
-            //    {
-            //        cliente.BaseAddress = new Uri("https://api.themoviedb.org/3/");
-            //        var responceTask = cliente.GetAsync("account/19728058/favorite/movies?api_key=c0387e98c8c3342904d47ecf8e243d5c");
-            //        responceTask.Wait();
-            //        var result = responceTask.Result;
-            //        if (result.IsSuccessStatusCode)
-            //        {
-            //            var readTask = result.Content.ReadAsStringAsync();
-            //            dynamic resultJson = JsonObject.Parse(readTask.Result.ToString());
-            //            readTask.Wait();
-            //            movi.Movies = new List<object>();
-            //            foreach (var resulItem in readTask.Result.)
-            //            {
+        //    using (var cliente = new HttpClient())
+        //    {
+        //        cliente.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+        //        var responceTask = cliente.GetAsync("account/19728058/favorite/movies?api_key=c0387e98c8c3342904d47ecf8e243d5c");
+        //        responceTask.Wait();
+        //        var result = responceTask.Result;
+        //        if (result.IsSuccessStatusCode)
+        //        {
+        //            var readTask = result.Content.ReadAsStringAsync();
+        //            dynamic resultJson = JsonObject.Parse(readTask.Result.ToString());
+        //            readTask.Wait();
+        //            movi.Movies = new List<object>();
+        //            foreach (var resulItem in readTask.Result.)
+        //            {
 
 
-            //                movi.Movies.Add(resulItem);
-            //            }
-            //        }
-            //    }
+        //                movi.Movies.Add(resulItem);
+        //            }
+        //        }
+        //    }
 
-            //    return View(movi);
+        //    return View(movi);
 
-        } 
+    } 
 }
