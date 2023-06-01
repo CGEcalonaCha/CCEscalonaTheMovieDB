ead4de61b6035fc09ec09013a493c5a0a8eeceef
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-06-01 17:54:49 -0600

    Grafica Bruto

---
+++ BL/Zona.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Zona
+    {
+        public static ML.Result GetAll()
+        {
+
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.CescalonaCineContext context = new DL.CescalonaCineContext())
+                {
+                    var query = context.Zonas.FromSqlRaw($"ZonaGetAll");
+
+                    result.Objects = new List<object>();
+
+                    foreach (var obj in query)
+                    {
+
+                        ML.Zona rol = new ML.Zona();
+
+                        rol.IdZona = obj.IdZona;
+                        rol.Nombre = obj.Nombre;
+
+                        result.Objects.Add(obj);
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DL/Cine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Cine
+{
+    public int IdCine { get; set; }
+
+    public string? Complejo { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public decimal? Venta { get; set; }
+
+    public int? IdZona { get; set; }
+
+    public virtual Zona? IdZonaNavigation { get; set; }
+    public string NombreZona { get; set; }
+}
