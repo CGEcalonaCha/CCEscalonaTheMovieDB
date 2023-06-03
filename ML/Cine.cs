097c7c0a77a3c6f8e48ee5636402c34385ec9007
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-06-02 18:25:40 -0600

    OK

--- ML/Cine.cs
+++ ML/Cine.cs
@@ -12,6 +12,8 @@ namespace ML
         public string Complejo { get; set; }
         public string Direccion { get; set; }
         public decimal Ventas { get; set; }
+        public int latitud { get; set; }
+        public int longitud { get; set; }
         public List<object> Cines { get; set; }
         public ML.Zona Zona { get; set; }
     }
